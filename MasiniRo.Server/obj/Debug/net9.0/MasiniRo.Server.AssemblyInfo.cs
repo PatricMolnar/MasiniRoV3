d968ac54f91673bb5fc9901e5ff1064d8473722d
PatricMolnar <molnarpatric@gmail.com>
2025-06-04 23:20:09 +0300

    update gitignore

--- MasiniRo.Server/obj/Debug/net9.0/MasiniRo.Server.AssemblyInfo.cs
+++ MasiniRo.Server/obj/Debug/net9.0/MasiniRo.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasiniRo.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4125b3129f32ed3a35e52f4cef81def350123e0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63cbf1def99e678112664aa8e8e2a5f6989c41b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasiniRo.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MasiniRo.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
